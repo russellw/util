b799d05442bd61c70e7bc85403743f2cec039054
Russell Wallace <russell.wallace@gmail.com>
2024-09-08 07:36:04 +0100

    isBinary

--- check-cr.go
+++ check-cr.go
@@ -3,20 +3,52 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const carriageReturn = byte(13)
 
-// isBinary checks if a file is binary by reading its content and checking for a high proportion of non-text characters.
-func isBinary(content []byte) bool {
-	for _, b := range content {
-		if b == 0 {
-			return true
-		}
+// isBinary detects if a file is binary by first checking its extension
+// against a known list of binary types, and then checking the entire file for null bytes.
+func isBinary(file string) bool {
+	// Known binary file extensions
+	binaryExtensions := map[string]bool{
+		".pdf": true, ".png": true, ".exe": true, ".jpg": true, ".jpeg": true,
+		".gif": true, ".bmp": true, ".zip": true, ".rar": true, ".tar": true,
+		".gz": true, ".7z": true, ".dll": true, ".iso": true, ".mp3": true,
+		".mp4": true, ".avi": true, ".mkv": true, ".mov": true, ".bin": true,
+		".dmg": true, ".class": true, ".so": true, ".o": true, ".obj": true,
 	}
+
+	// Get file extension and convert to lowercase for case-insensitive comparison
+	ext := strings.ToLower(filepath.Ext(file))
+	if binaryExtensions[ext] {
+		return true
+	}
+
+	// Open the file for reading
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err) // Crash the program on error
+	}
+	defer f.Close()
+
+	// Read the entire contents of the file
+	data, err := ioutil.ReadAll(f)
+	if err != nil && err != io.EOF {
+		log.Fatal(err) // Crash the program on error
+	}
+
+	// Check for null bytes in the entire file
+	if bytes.IndexByte(data, 0) != -1 {
+		return true
+	}
+
 	return false
 }
 
@@ -31,15 +63,15 @@ func processFile(path string, info os.FileInfo) error {
 		return nil
 	}
 
+	// Check if the file is binary
+	if isBinary(path) {
+		return nil
+	}
+
 	// Read file content
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
 		return err
-	}
-
-	// Check if the file is binary
-	if isBinary(content) {
-		return nil
 	}
 
 	// Check for carriage return character
