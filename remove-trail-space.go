82fdcb6507839110eecc283f5b53aa36c61d798a
Russell Wallace <russell.wallace@gmail.com>
2024-09-09 09:02:17 +0100

    refactor

--- remove-trail-space.go
+++ remove-trail-space.go
@@ -2,55 +2,14 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 	"unicode"
 )
-
-// isBinary detects if a file is binary by first checking its extension
-// against a known list of binary types, and then checking the entire file for null bytes.
-func isBinary(file string) bool {
-	// Known binary file extensions
-	binaryExtensions := map[string]bool{
-		".pdf": true, ".png": true, ".exe": true, ".jpg": true, ".jpeg": true,
-		".gif": true, ".bmp": true, ".zip": true, ".rar": true, ".tar": true,
-		".gz": true, ".7z": true, ".dll": true, ".iso": true, ".mp3": true,
-		".mp4": true, ".avi": true, ".mkv": true, ".mov": true, ".bin": true,
-		".dmg": true, ".class": true, ".so": true, ".o": true, ".obj": true,
-	}
-
-	// Get file extension and convert to lowercase for case-insensitive comparison
-	ext := strings.ToLower(filepath.Ext(file))
-	if binaryExtensions[ext] {
-		return true
-	}
-
-	// Open the file for reading
-	f, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err) // Crash the program on error
-	}
-	defer f.Close()
-
-	// Read the entire contents of the file
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatal(err) // Crash the program on error
-	}
-
-	// Check for null bytes in the entire file
-	if bytes.IndexByte(data, 0) != -1 {
-		return true
-	}
-
-	return false
-}
 
 // Removes trailing whitespace from a string.
 func trimTrailingWhitespace(line string) string {
@@ -105,7 +64,7 @@ func processFiles(files []string, writeChanges bool) error {
 	for _, file := range files {
 		err := processFile(file, writeChanges)
 		if err != nil {
-			fmt.Printf("Error processing file %s: %v\n", file, err)
+			log.Fatal(err)
 		}
 	}
 	return nil
@@ -116,12 +75,11 @@ func main() {
 	flag.Parse()
 
 	if len(flag.Args()) == 0 {
-		fmt.Println("Usage: remove-trail-space [-w] file1 [file2 ...]")
-		return
+		log.Fatal("Usage: remove-trail-space [-w] file1 [file2 ...]")
 	}
 
 	err := processFiles(flag.Args(), *writeFlag)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		log.Fatal(err)
 	}
 }
