f9be9fd87eb7dd4a436a219ceb601f3fb6c8deec
Russell Wallace <russell.wallace@gmail.com>
2024-09-09 23:10:38 +0100

    refactor

--- sort-fns-js.go
+++ sort-fns-js.go
@@ -11,20 +11,22 @@ import (
 	"strings"
 )
 
+var writeBack bool
+
 func main() {
-	writeBack := flag.Bool("w", false, "write modified files back to disk")
+	flag.BoolVar(&writeBack, "w", false, "write modified files back to disk")
 	flag.Parse()
 	files := flag.Args()
 	if len(files) == 0 {
 		log.Fatal("No files were specified")
 	}
 	for _, file := range files {
-		processFile(file, *writeBack)
+		processFile(file)
 	}
 }
 
-func processFile(path string, writeBack bool) {
-	if filepath.Ext(path) != ".js" {
+func processFile(file string) {
+	if filepath.Ext(file) != ".js" {
 		return
 	}
 }
