d43e9865799bddb2af3a7a4a33678f75212af26b
Russell Wallace <russell.wallace@gmail.com>
2024-09-09 22:37:43 +0100

    check for directories or files with non-ASCII characters in their names

---
+++ check-unicode-names.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"unicode"
+)
+
+// isASCII checks if all characters in the string are ASCII.
+func isASCII(s string) bool {
+	for _, r := range s {
+		if r > unicode.MaxASCII {
+			return false
+		}
+	}
+	return true
+}
+
+// checkDir recursively checks directories for files or subdirectories with non-ASCII names.
+func checkDir(path string) error {
+	err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// Check if the file or directory name contains non-ASCII characters
+		if !isASCII(info.Name()) {
+			fmt.Println(p)
+		}
+		return nil
+	})
+	return err
+}
+
+func main() {
+	// Replace this with the directory path you want to start from
+	rootDir := "."
+
+	err := checkDir(rootDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
