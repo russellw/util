ef1efa6387a9883bbba54dfd8d729bb8b7c93d47
Russell Wallace <russell.wallace@gmail.com>
2024-09-09 12:39:25 +0100

    optimize

--- common.go
+++ common.go
@@ -9,17 +9,18 @@ import (
 	"strings"
 )
 
+// Known binary file extensions
+var binaryExtensions = map[string]bool{
+	".pdf": true, ".png": true, ".exe": true, ".jpg": true, ".jpeg": true,
+	".gif": true, ".bmp": true, ".zip": true, ".rar": true, ".tar": true,
+	".gz": true, ".7z": true, ".dll": true, ".iso": true, ".mp3": true,
+	".mp4": true, ".avi": true, ".mkv": true, ".mov": true, ".bin": true,
+	".dmg": true, ".class": true, ".so": true, ".o": true, ".obj": true,
+}
+
 // isBinary detects if a file is binary by first checking its extension
 // against a known list of binary types, and then checking the entire file for null bytes.
 func isBinary(file string) bool {
-	// Known binary file extensions
-	binaryExtensions := map[string]bool{
-		".pdf": true, ".png": true, ".exe": true, ".jpg": true, ".jpeg": true,
-		".gif": true, ".bmp": true, ".zip": true, ".rar": true, ".tar": true,
-		".gz": true, ".7z": true, ".dll": true, ".iso": true, ".mp3": true,
-		".mp4": true, ".avi": true, ".mkv": true, ".mov": true, ".bin": true,
-		".dmg": true, ".class": true, ".so": true, ".o": true, ".obj": true,
-	}
 
 	// Get file extension and convert to lowercase for case-insensitive comparison
 	ext := strings.ToLower(filepath.Ext(file))
