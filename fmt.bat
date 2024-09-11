21d18320bf8bed383871486241ff5a4fbc230da2
Russell Wallace <russell.wallace@gmail.com>
2024-09-11 12:10:17 +0100

    fmt

--- fmt.bat
+++ fmt.bat
@@ -1,6 +1,14 @@
+rem C and C++
 clang-format -i --style=file *.c *.cpp||exit /b
+
+rem Go
 for %%x in (*.go) do gofmt -s -w %%x||exit /b
 
-do-all-recur . comment-space -w
+rem JavaScript
+call prettier --no-semi --print-width 132 -w .||exit /b
 
+rem All the above
+do-all-recur . comment-space -w||exit /b
+
+rem Check the results
 git diff
