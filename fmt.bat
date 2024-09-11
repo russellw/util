19f48090a4b1a970a74b50bd304b84bc7dd3d993
Russell Wallace <russell.wallace@gmail.com>
2024-09-11 15:41:47 +0100

    fmt

--- fmt.bat
+++ fmt.bat
@@ -3,6 +3,7 @@ clang-format -i --style=file *.c *.cpp||exit /b
 
 rem Go
 gofmt -s -w .||exit /b
+do-all-recur . sort-fns-go -w||exit /b
 
 rem JavaScript
 call prettier --no-semi --print-width 132 -w .||exit /b
