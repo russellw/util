a644d31f4aea5cc3c13baf11cf617fcf6d29e5ae
Russell Wallace <russell.wallace@gmail.com>
2024-09-11 16:11:47 +0100

    fmt

--- fmt.bat
+++ fmt.bat
@@ -6,7 +6,7 @@ gofmt -s -w .||exit /b
 do-all-recur . sort-fns-go -w||exit /b
 
 rem JavaScript
-call prettier --no-semi --print-width 132 -w .||exit /b
+rem call prettier --no-semi --print-width 132 -w .||exit /b
 
 rem All the above
 do-all-recur . comment-space -w||exit /b
