455506104690ebce95b126dfabf478bacfeb0d9d
Russell Wallace <russell.wallace@gmail.com>
2025-02-19 00:13:19 +0000

    Added 1 file(s): examples/leak-stoull.cc

---
+++ examples/leak-stoull.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+using std::string;
+
+int main() {
+    size_t x = stoull(string("123"));
+    return 0;
+}
