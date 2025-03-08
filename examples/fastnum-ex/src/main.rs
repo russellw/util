cd0de68f892b70cb22d6dc532947c77d70979009
Russell Wallace <russell.wallace@gmail.com>
2025-03-08 21:32:28 +0000

    Modified 1 file(s): examples/fastnum-ex/src/main.rs

--- examples/fastnum-ex/src/main.rs
+++ examples/fastnum-ex/src/main.rs
@@ -1,3 +1,11 @@
+use fastnum::{udec256, UD256};
+
 fn main() {
+    let a = udec256!(0.1);
+    let b = udec256!(0.2);
+
+    assert_eq!(a + b, udec256!(0.3));
+
     println!("Hello, world!");
+    println!("{}", a);
 }
