7722a64627c0f7ac578cde0393e94a787ac15c61
Russell Wallace <russell.wallace@gmail.com>
2025-03-10 01:39:24 +0000

    Added 1 file(s): examples/vm/src/vm.rs; Modified 1 file(s): examples/vm/src/main.rs

---
+++ examples/vm/src/vm.rs
@@ -0,0 +1,89 @@
+use fastnum::decimal::Context;
+use fastnum::{D256};
+use std::fmt;
+use std::ops::{Add, Div, Mul, Sub};
+use std::rc::Rc;
+
+#[derive(Clone, Debug)]
+pub enum Value {
+    Number(D256),
+    String(Rc<String>),
+}
+
+pub type EvalResult = Result<Value, String>;
+
+pub const NO_TRAPS: Context = Context::default().without_traps();
+
+impl Value {
+    pub fn number(a: D256) -> Self {
+        Value::Number(a)
+    }
+
+    pub fn string<S: Into<String>>(s: S) -> Self {
+        Value::String(Rc::new(s.into()))
+    }
+
+    pub fn as_string(&self) -> String {
+        match self {
+            Value::String(s) => s.to_string(),
+            Value::Number(a) => a.to_string(),
+        }
+    }
+}
+
+impl Add for Value {
+    type Output = EvalResult;
+
+    fn add(self, other: Value) -> EvalResult {
+        match (&self, &other) {
+            (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() + b.clone())),
+            _ => {
+                    let mut result = self.as_string();
+                    result.push_str(&other.as_string());
+                    Ok(Value::String(Rc::new(result)))
+                }
+            }
+        }
+    }
+
+impl Div for Value {
+    type Output = EvalResult;
+
+    fn div(self, other: Value) -> EvalResult {
+        match (&self, &other) {
+            (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() / b.clone())),
+            _ => Err("/: expected numbers".to_string()),
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = EvalResult;
+
+    fn mul(self, other: Value) -> EvalResult {
+        match (&self, &other) {
+            (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() * b.clone())),
+            _ => Err("*: expected numbers".to_string()),
+        }
+    }
+}
+
+impl Sub for Value {
+    type Output = EvalResult;
+
+    fn sub(self, other: Value) -> EvalResult {
+        match (&self, &other) {
+            (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a.clone() - b.clone())),
+            _ => Err("-: expected numbers".to_string()),
+        }
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Number(a) => write!(f, "{}", a),
+            Value::String(s) => write!(f, "{}", s),
+        }
+    }
+}
