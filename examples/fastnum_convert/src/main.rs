cfe865a45e89cc387058321e83de8549db713e7e
Russell Wallace <russell.wallace@gmail.com>
2025-03-12 05:44:55 +0000

    Modified 1 file(s): examples/fastnum_convert/src/main.rs

--- examples/fastnum_convert/src/main.rs
+++ examples/fastnum_convert/src/main.rs
@@ -1,8 +1,13 @@
-use fastnum::dec256;
+use fastnum::{D256, dec256};
+use num_traits::FromPrimitive;
 use num_traits::ToPrimitive;
 
 fn main() {
     let a = dec256!(10);
     let b = a.to_u128();
     println!("{:?}", b);
+
+    let n = 340_282_366_920_938_463_463_374_607_431_768_211_455u128;
+    let z = D256::from_u128(n).unwrap();
+    println!("{}", z);
 }
