f5d4a765ffd3c3fe03336c6a2456b68d354838ec
Russell Wallace <russell.wallace@gmail.com>
2025-03-13 03:27:47 +0000

    Added 1 file(s): examples/runtime_error.py; Modified 1 file(s): scrap.rs

--- scrap.rs
+++ scrap.rs
@@ -6,3 +6,17 @@ fn report_char(c: char) -> String {
         format!("\\u{:x}", c as u32)
     }
 }
+
+    fn peek_eq(&self) -> bool {
+        let mut i = self.pos;
+        while self.chars[i] == ' ' || self.chars[i] == '\t' {
+            i += 1;
+        }
+        if self.chars[i] != '=' {
+            return false;
+        }
+        match self.chars[i + 1] {
+            '=' | '>' | '<' => false,
+            _ => true,
+        }
+    }
