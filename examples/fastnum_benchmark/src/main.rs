ea235fad18ced97b6e9cb9f16b1125749db2833d
Russell Wallace <russell.wallace@gmail.com>
2025-03-14 02:32:02 +0000

    Modified 1 file(s): examples/fastnum_benchmark/src/main.rs

--- examples/fastnum_benchmark/src/main.rs
+++ examples/fastnum_benchmark/src/main.rs
@@ -6,7 +6,7 @@ use num_traits::ToPrimitive;
 fn main() {
 	let mut t=dec256!(0);
 	for i in 0..10000 {
-			t+=D256::from_i32(i).unwrap().sqrt();
+			t+=D256::from_i32(i).unwrap().sin();
     }
     println!("{}",t);
 }
