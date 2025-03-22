aa016694a7ca097c3805a528181e507c26de35e9
Russell Wallace <russell.wallace@gmail.com>
2025-03-22 11:17:05 +0000

    Added 3 file(s): examples/line_len/Cargo.toml, examples/line_len/log.txt, examples/line_len/src/main.rs

---
+++ examples/line_len/src/main.rs
@@ -0,0 +1,13 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("log.txt")?;
+    let mut reader = BufReader::new(f);
+
+    let mut line = String::new();
+    let len = reader.read_line(&mut line)?;
+    println!("First line is {len} bytes long");
+    Ok(())
+}
