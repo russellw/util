55b119f84c151afd8a3386e0c0eede0f1b5e44dd
Russell Wallace <russell.wallace@gmail.com>
2025-03-22 11:18:15 +0000

    Modified 1 file(s): examples/line_len/src/main.rs

--- examples/line_len/src/main.rs
+++ examples/line_len/src/main.rs
@@ -1,6 +1,6 @@
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
     let f = File::open("log.txt")?;
@@ -8,6 +8,8 @@ fn main() -> std::io::Result<()> {
 
     let mut line = String::new();
     let len = reader.read_line(&mut line)?;
+
+    // This may or may not include a newline character depending on the input
     println!("First line is {len} bytes long");
     Ok(())
 }
