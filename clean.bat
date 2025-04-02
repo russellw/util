edbc8c9d0fb9184364246dbcb521178f13c4f0ae
Russell Wallace <russell.wallace@gmail.com>
2025-04-02 15:59:37 +0100

    Modified 1 file(s): clean.bat

--- clean.bat
+++ clean.bat
@@ -1,2 +1,10 @@
 del /s *.exe
 del /s *.obj
+
+for /d /r %%d in (*) do (
+    if exist "%%d\target\" (
+        echo Found target directory in "%%d"
+        rmdir /s /q "%%d\target"
+        echo Deleted target directory in "%%d"
+    )
+)
