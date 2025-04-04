483a2fe6deae0976e7b4e300ed8ecd6ec31ed5e4
Russell Wallace <russell.wallace@gmail.com>
2025-04-04 15:50:06 +0100

    Added 2 file(s): examples/readline_test.js, examples/test.js

---
+++ examples/readline_test.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter some text: ', (input) => {
+  console.log(`You entered: ${input}`);
+  rl.close();
+});
---
+++ examples/test.js
@@ -0,0 +1,24 @@
+function f(){
+}
+
+console.log(f())
+
+
+for (a in 'abc') {
+console.log(a)
+}
+
+for (a of 'abc') {
+console.log(a)
+}
+
+for (a of [30,40,50]) {
+console.log(a)
+}
+
+m=new Map([ ['a',1], ['b',2] ])
+console.log(m)
+
+for (a of m) {
+console.log(a)
+}
